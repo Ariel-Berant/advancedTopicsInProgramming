84a0a2fa6aaa52eda14edeae9487e3958e89d6f9
Ariel-Berant <berant@mail.tau.ac.il>
2025-05-30 19:06:04 +0300

    Algorithm should theoretically not account for bullet difrection & work

--- Ex2/projFiles/unique/PlayerTankAlgorithm.cpp
+++ Ex2/projFiles/unique/PlayerTankAlgorithm.cpp
@@ -89,7 +89,7 @@ bool PlayerTankAlgorithm::isSafe(const int col, const int row,
     };
     for (array<int, 3> loc: possibleLocs) {
         bulletObj = tankBattleInfo->getGameBoard()[loc[0]][loc[1]][1].get();
-        if (bulletObj && dynamic_cast<bullet *>(bulletObj) && dynamic_cast<bullet *>(bulletObj)->getOrientation() == loc[2]) {
+        if (bulletObj && dynamic_cast<bullet *>(bulletObj) /*&& dynamic_cast<bullet *>(bulletObj)->getOrientation() == loc[2]*/) {
             bulletNotFound = false;
         }
     }
