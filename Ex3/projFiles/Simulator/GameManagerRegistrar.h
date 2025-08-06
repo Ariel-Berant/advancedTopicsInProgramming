9591739754d34dc5319cfdf756fb2f309019ef4a
Ariel-Berant <berant@mail.tau.ac.il>
2025-08-06 19:47:15 +0300

    Started adding registrar for GameManager

---
+++ Ex3/projFiles/Simulator/GameManagerRegistrar.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <vector>
+#include "GameManagerRegistration.h"
+
+namespace Simulator_0000
+{
+    class GameManagerRegistrar
+    {
+    public:
+        class GameManagerWrapper{
+            public:
+        };
+    };
+} // namespace Simulator_0000
--- Ex3/projFiles/Simulator/Simulator.h
+++ Ex3/projFiles/Simulator/Simulator.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include "AlgorithmRegistrar.h"
+
+
+namespace Simulator_0000
+{
+    class Simulator
+    {
+    private:
+        int threadsnum;
+        int maxsteps;
+        int numplayers;
+    public:
+        Simulator(/* args */);
+        ~Simulator();
+    };
+    
+    Simulator::Simulator(/* args */)
+    {
+    }
+    
+    Simulator::~Simulator()
+    {
+    }
+    
+    
+}
